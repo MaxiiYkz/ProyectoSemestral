1c474404ac20c08636674e11dab9364aa7d669b4
MaxiiYkz <max.paez@duocuc.cl>
2025-10-20 22:26:13 -0300

    V0.1

---
+++ app/src/main/java/com/example/proyectosemestral/ui/data/AppState.kt
@@ -0,0 +1,54 @@
+package com.example.proyectosemestral.ui.data
+
+import androidx.compose.runtime.mutableStateListOf
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+data class Usuario(val email: String, val contraseña: String)
+
+class AppState(private val dataStore: DataStoreManager){
+    val usuarios = mutableStateListOf<Usuario>()
+    val usuarioActual: Usuario? = null
+    
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    fun cargarDatos(){
+        scope.launch{
+            val users = dataStore.getUsers().first()
+
+            usuarios.clear()
+            usuarios.addAll(users)
+
+        }
+    }
+
+    fun registrarUsuario(email: String, contraseña: String): Boolean{
+        if (usuarios.any{ it.email == email}) return false
+        val nuevo = Usuario(email, contraseña)
+        usuario.add(nuevo)
+        guardarUsuarios()
+        return true
+    }
+
+    fun login(email: String, contraseña: String): Boolean{
+        var user = usuarios.find { it.email == email && it.contraseña == contraseña }
+        return if ( user != null){
+            usuarioActual = user
+            true
+        }else false
+    }
+
+    fun logout(){
+        usuarioActual = null
+    }
+
+    private fun guardarUsuarios(){
+        scope.launch {
+            dataStore.saveUsers(usuarios)
+        }
+    }
+
+
+
+}
