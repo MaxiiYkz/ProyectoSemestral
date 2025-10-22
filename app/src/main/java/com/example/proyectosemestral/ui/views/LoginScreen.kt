ac0983884b4d29cf37d7a1519960b5d32c5d0321
Vina del Mar <vina@duoc.acad>
2025-10-22 12:30:36 -0300

    V0.7

--- app/src/main/java/com/example/proyectosemestral/ui/views/LoginScreen.kt
+++ app/src/main/java/com/example/proyectosemestral/ui/views/LoginScreen.kt
@@ -2,18 +2,24 @@ package com.example.proyectosemestral.ui.views
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.LockOpen
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Shield
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -23,7 +29,9 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -61,15 +69,43 @@ fun LoginScreen(navController: NavHostController, appState: AppState){
                 .fillMaxSize()
                 .padding(padding)
                 .padding(16.dp),
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ){
+
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Logo",
+                modifier = Modifier.size(100.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+
+            Text(
+                text = "Bienvenido",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(Modifier.height(4.dp))
+
+            Text(
+                text = "Inicia sesión para continuar",
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+            Spacer(Modifier.height(32.dp))
+
             OutlinedTextField(
                 value = usuario,
                 onValueChange = { usuario = it },
                 label = { Text("Usuario") },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(Icons.Default.Person, contentDescription = "Icono de Usuario")
+                }
             )
-            Spacer(Modifier.height(8.dp))
+            Spacer(Modifier.height(16.dp))
+
             OutlinedTextField(
                 value = contrasena,
                 onValueChange = { contrasena = it },
@@ -90,7 +126,8 @@ fun LoginScreen(navController: NavHostController, appState: AppState){
                     }
                 }
             )
-            Spacer(Modifier.height(16.dp))
+            Spacer(Modifier.height(24.dp))
+
             if(error.isNotEmpty()){
                 Text(error, color = MaterialTheme.colorScheme.error)
                 Spacer(Modifier.height(8.dp))
@@ -111,14 +148,22 @@ fun LoginScreen(navController: NavHostController, appState: AppState){
                     }
                 },
                 modifier = Modifier.fillMaxWidth()
+                    .height(50.dp),
+                        shape = RoundedCornerShape(50)
             ) {
                 Text("Iniciar Sesión")
             }
-            Spacer(Modifier.height(8.dp))
-            TextButton(onClick =  { navController.navigate("registro")}) {
-                Text("¿No tienes cuenta? Regístrate Aquí")
-            }
+            Spacer(Modifier.height(16.dp))
 
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(onClick =  { navController.navigate("registro")}) {
+                    Text("¿No tienes cuenta? Regístrate Aquí")
+                }
+            }
         }
     }
 }
