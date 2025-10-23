43e5b4a1d659a8923f03b98d7a1dccf4ba62aef1
MaxiiYkz <max.paez@duocuc.cl>
2025-10-22 22:42:23 -0300

    V1.0

---
+++ app/src/main/java/com/example/proyectosemestral/ui/navigation/Navigation.kt
@@ -0,0 +1,21 @@
+package com.example.proyectosemestral.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.example.proyectosemestral.ui.data.AppState
+import com.example.proyectosemestral.ui.views.*
+
+@Composable
+fun AppNavigation(navController: NavHostController, appState: AppState){
+
+    NavHost(
+        navController = navController, startDestination = "login",
+    ){
+        composable("login") { LoginScreen(navController, appState) }
+        composable("registro") { RegistroScreen(navController, appState) }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/proyectosemestral/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.proyectosemestral.ui.theme
+
+import androidx.compose.ui.graphics.Color
+val Green80 = Color(0xFFB1DDA0)
+val GreenGrey80 = Color(0xFFBFCBBA)
+val Teal80 = Color(0xFFA0D0C6)
+val Green40 = Color(0xFF4B6341)
+val GreenGrey40 = Color(0xFF596155)
+val Teal40 = Color(0xFF3E635B)
